25e80da7c214a6350249b608a1fa7c67dab96d36
Robbin Tapper <robbin.tapper@mobenga.com>
2015-08-31 11:43:42 +0200

    Plankton v2.2beta - Testing iPhone with speech

---
+++ scripts/utils.js
@@ -0,0 +1,17 @@
+function iOS() {
+
+    var iDevices = [
+        'iPad Simulator',
+        'iPhone Simulator',
+        'iPod Simulator',
+        'iPad',
+        'iPhone',
+        'iPod'
+    ];
+
+    while (iDevices.length) {
+        if (navigator.platform === iDevices.pop()){ return true; }
+    }
+
+    return false;
+}
