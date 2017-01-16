d7f597fc8057bd8227110a319c4e173b33b2747f
Robbin Tapper <robbin.tapper@mobenga.com>
2017-01-16 14:39:15 +0100

    Fix for fix

--- help.js
+++ help.js
@@ -1,10 +1,10 @@
 console.info(`Usage:
 
-npm start                          - Will start on port 3000)
-npm run plankton                   - Will start on port 3000)
+npm start                          - Will start on port 3000
+npm run plankton                   - Will start on port 3000
 
-npm start -- --port=1337           - Will start on port 1337)
-npm run plankton -- --port=1337    - Will start on port 1337)
+npm start -- --port=1337           - Will start on port 1337
+npm run plankton -- --port=1337    - Will start on port 1337
 
-npm run help                       - Displays this help)
+npm run help                       - Displays this help
 `)
